1d1cdc8bc3db123bd65cf461c8018f16161d3741
Clifford Phan <clifford.phan@gmail.com>
2017-10-31 02:44:06 +0000

    Initialised react es6 into project

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import './css/index.css';
+// import Header from './header';
+// import Content from './content';
+// import Footer from './footer'
+import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<Header />, document.getElementById('header'));
+// ReactDOM.render(<Content />, document.getElementById('content'));
+// ReactDOM.render(<Footer />, document.getElementById('footer'));
+
+registerServiceWorker();
