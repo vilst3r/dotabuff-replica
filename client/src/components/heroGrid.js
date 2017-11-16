74f35e7774e8c2e412a699ba6489fa5dd4bda3ce
Clifford Phan <clifford.phan@gmail.com>
2017-11-16 02:34:37 +0000

    Implemented arrow functions for the hero grid

--- client/src/components/heroGrid.js
+++ client/src/components/heroGrid.js
@@ -17,22 +17,24 @@ export default class heroGrid extends Component {
     return (
         <div className = "panel-body">
         {
-            this.state.heroes.sort(function(a, b) {
-              return (a.localized_name < b.localized_name) ? -1 : (a.localized_name > b.localized_name) ? 1 : 0;
-            }) 
-            ?
-            
-            this.state.heroes.map(function(item, key) {
-                return (
-                    <a href={'heroes/' + item.localized_name }>
-                        <div className="hero col-xs-4 col-sm-3 col-md-2 col-lg-1">
-                            <img className="img-responsive" src = {'https://api.opendota.com' + item.img }/>
-                            <div className="title"> { item.localized_name } </div>
-                        </div> 
-                    </a>
-                )                    
-            })
+          this.state.heroes.sort( (a, b) =>
+            (a.localized_name < b.localized_name) ?
+              -1
             :
+              (a.localized_name > b.localized_name) ? 1 : 0
+          ) ?
+          
+            this.state.heroes.map((item, key) =>
+              (
+                <a key ={key} href={'heroes/' + item.localized_name }>
+                    <div className="hero col-xs-4 col-sm-3 col-md-2 col-lg-1">
+                        <img className="img-responsive" src = {'https://api.opendota.com' + item.img }/>
+                        <div className="title"> { item.localized_name } </div>
+                    </div> 
+                </a>              
+              )
+            )
+          :
             null
         }        
         </div>        
