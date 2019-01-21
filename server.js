e57719236286bc5da7c751aff39892b985498dec
vilst3r <clifford.phan@gmail.com>
2019-01-21 18:54:13 +1100

    Update server to ES6 syntax + add babel + fix vulnerabilities in dependencies

--- server.js
+++ server.js
@@ -1,71 +1,78 @@
-const config = require('./config');
+/* Configuration */
+import { app, api } from './config';
 
 /* Modules */
-const express = require('express');
-const logger = require('morgan');
-const path = require('path');
-const fs = require('fs');
-const chalk = require('chalk');
-const errorHandler = require('errorhandler');
-const bodyParser = require('body-parser');
-const favicon = require('serve-favicon');
-const cors = require('cors');
-const dotaWebAPI = require('dota2_web_api');
+import { dotaWebAPI } from 'dota2_web_api';
+import express from 'express';
+import morgan from 'morgan';
+import path from 'path';
+import fs from 'fs';
+import chalk from 'chalk';
+import errorHandler from 'errorhandler';
+import bodyParser from 'body-parser';
+import favicon from 'serve-favicon';
+import cors from 'cors';
+import { format } from 'util';
 
 /* Routes */
-const index = require('./routes/index');
-const heroes = require('./routes/heroes');
+import index from './routes/index';
+import heroes from './routes/heroes';
 
-const app = express();
+const server = express()
 
-/*
-  Express Configuration
-*/
+/**
+ * Express configuration
+ */
 const logDate = new Date()
-app.set('host', config.app.host)
-app.set('port', config.app.port)
-app.set('env', config.app.env)
-app.set('api', new dotaWebAPI(config.api.key))
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+server.set('host', app.host)
+server.set('port', app.port)
+server.set('env', app.env)
+server.set('api', new dotaWebAPI(api.key))
 
-/*
-  Registering Middleware
-*/
-app.use(bodyParser.json());
-app.use(logger('common', {
-  stream: fs.createWriteStream('./log/log_' + logDate.getDate() + '-' + logDate.getMonth() + 1 + '-' + logDate.getFullYear() + '.log', {flags: 'a'})
+/* view engine setup */
+server.set('views', path.join(__dirname, 'views'));
+server.set('view engine', 'jade');
+
+/**
+ * Registering Middleware
+ */
+server.use(bodyParser.json());
+server.use(morgan('common', {
+  stream: fs.createWriteStream(format('./log/%d-%d-%d.log', logDate.getDate(), logDate.getMonth() + 1, logDate.getFullYear()), {
+    flags: 'a'
+  })
 }));
-app.use(logger('dev'));
-app.use(bodyParser.json());
+server.use(morgan('dev'));
+server.use(bodyParser.json());
+
 // enabling cors
-// app.use(cors({
-//   origin: 'http://localhost:8080',
-//   optionsSuccessStatus: 200         // some legacy browsers (IE11, various SmartTVs) choke on 204 
-// }))
+server.use(cors({
+  origin: 'http://localhost:8080',
+  optionsSuccessStatus: 200         // some legacy browsers (IE11, various SmartTVs) choke on 204 
+}))
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 // app.use(express.static(path.join(__dirname, 'public')));
 
-/*
-  Loading Route Middleware
-*/
-app.use('/', index)
-app.use('/heroes', heroes)
+/**
+ * Loading Route Middleware
+ */
+server.use('/', index)
+server.use('/heroes', heroes)
 
-/*
-  Error Handler for development
-*/
-if (app.get('env') === 'development') {
-  app.use(errorHandler());
+/**
+ * Error Handler for development 
+ */
+if (app.env === 'development') {
+  server.use(errorHandler());
 }
 
-/*
-  Start to listen for the Express server
-*/
-app.listen(config.app.port, () => {
-  console.log('App is running at http://localhost:%d in %s environment', app.get('port'), app.get('env'));
+
+/**
+ * Start to listen for the Express server
+ */
+server.listen(app.port, () => {
+  console.log('App is running at http://localhost:%d in %s environment', app.port, app.env);
   console.log('Press CTRL-C to stop\n');
 })
 
-module.exports = app
+module.exports = server
