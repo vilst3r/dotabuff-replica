db4773209f5859011b018762d315cdfdfde08cd9
vilst3r <clifford.phan@gmail.com>
2019-01-24 19:59:58 +1100

    Complete proper caching for hero services

--- services/hero.service.js
+++ services/hero.service.js
@@ -2,8 +2,9 @@ import fs from 'fs'
 
 class HeroService {
 	getHeroes(api) {
+		let result
 		try {
-			const result = fs.readFileSync('cached_data/heroes.json')
+			result = fs.readFileSync('cached_data/heroes.json')
 			return new Promise((resolve) => resolve(JSON.parse(result)))
 		} catch(err) {
 			console.log("Writing data to cache directory")
@@ -23,38 +24,47 @@ class HeroService {
 				.sort((a, b) => a.localized_name < b.localized_name ? -1 : a.localized_name == b.localized_name ? 0 : 1)
 			)
 			.then(heroes => {
+				result = heroes
+				let hashMap = {}
+				
+				heroes.map(hero => hashMap[hero.id] = hero)
+				return hashMap
+			})
+			.then(hashMap => {
 				fs.mkdir('cached_data', err => err ? console.log('Writing directory failed!\n' + err) : null)
 				fs.writeFile('cached_data/heroes.json', 
-					JSON.stringify(heroes, null, 4), 
+					JSON.stringify(hashMap, null, 4), 
 					(err => err ? console.log('Writing file failed!\n' + err) : null)
 				)
-				return heroes
+				return result
 			})
 			.catch(error => error)
 		}
 	}
 
 	getHero(api, heroId) {
-		return api.getHeroes()
-		.then(body => body.result.heroes.filter(hero => hero.id == heroId))
-		.then(hero => hero[0])
-		.then(hero => {
-			hero.icon_url = api.getHeroIconPath(hero.name, "full.png")
-			hero.name = hero.name.replace(/npc_dota_hero_/g, '').replace(/_/g, ' ')
-			hero.localized_name = hero.name.replace(/npc_dota_hero_/g, '').replace(/_/g, ' ')
-					.split(' ').map(word => 
-						word.charAt(0).toUpperCase() + word.slice(1)
-					).join(' ')
-			return hero
-		})
-		.catch(error => error)
-	}
-}
+		let result
+		try {
+			result = fs.readFileSync('cached_data/heroes.json')
+			return new Promise((resolve) => resolve(JSON.parse(result)[heroId]))
+		} catch(err) {
+			console.log("Data for hero_id:%d is not cached - making call to SDK with given id", heroId)
 
-// Use for caching data
-const writeToDisk = (body) => {
-	const data = body.map(hero => ({name: hero.name, localized_name: hero.localized_name}))
-	fs.writeFile('cached_data/heroes.json', JSON.stringify(data, null, 4), (() => console.log('error writing')))
+			return api.getHeroes()
+			.then(body => body.result.heroes.filter(hero => hero.id == heroId))
+			.then(hero => hero[0])
+			.then(hero => {
+				hero.icon_url = api.getHeroIconPath(hero.name, "full.png")
+				hero.name = hero.name.replace(/npc_dota_hero_/g, '').replace(/_/g, ' ')
+				hero.localized_name = hero.name.replace(/npc_dota_hero_/g, '').replace(/_/g, ' ')
+						.split(' ').map(word => 
+							word.charAt(0).toUpperCase() + word.slice(1)
+						).join(' ')
+				return hero
+			})
+			.catch(error => error)
+		}
+	}
 }
 
 export default HeroService
